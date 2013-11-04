96dd31c0805ea6801f39d85d079af1db8db54e50
Patrick Steiger <psteiger@gmail.com>
2013-11-04 11:07:17 -0300

    criado modelos

--- gamif/models.py
+++ gamif/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Funcao(models.Model):
+	nome = models.CharField(max_length=100)
+
+class Funcionario(models.Model):
+	nome = models.CharField(max_length=100)
+	funcao = models.ForeignKey(Funcao)
+	equipe = models.ForeignKey(Equipe)
+	foto = models.ImageField(upload_to='/tmp')
+
+class Equipe(models.Model):
+	nome = models.CharField(max_length=100)
+	responsavel = models.ForeignKey(Funcionario)
+
+class Regra(models.Model):
+	peso = models.IntegerField()
+	nome = models.CharField(max_length=100)
+	
